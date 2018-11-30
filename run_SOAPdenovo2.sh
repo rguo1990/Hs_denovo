659875d47391347b048993aa2f0a706bc257eda3
Rong Guo <33555113+rguo1990@users.noreply.github.com>
2018-11-30 13:50:35 -0500

    Update run_SOAPdenovo2.sh

--- run_SOAPdenovo2.sh
+++ run_SOAPdenovo2.sh
@@ -30,17 +30,17 @@ cd ~/data/SOAPdenovo2
 ./SOAPdenovo-127mer all -s ../soap.contig.txt -p 48 -K 39 -R -o ~/data/Kmer39/M1/graph_391 1>~/data/Kmer39/M1/ass.log 2>~/data/Kmer39/M1/ass.err
 ./SOAPdenovo-127mer all -s ../soap.contig.txt -p 48 -K 39 -R -M 2 -o ~/data/Kmer39/M2/graph_392 1>~/data/Kmer39/M2/ass.log 2>~/data/Kmer39/M2/ass.err
 ./SOAPdenovo-127mer all -s ../soap.contig.txt -p 48 -K 39 -R -M 3 -o ~/data/Kmer39/M3/graph_393 1>~/data/Kmer39/M3/ass.log 2>~/data/Kmer39/M3/ass.err
-./SOAPdenovo-127mer all -s ../soap.contig.txt -p 48 -K 41 -R -o ~/data/Kmer41/graph_41 1>~/data/Kmer41/ass.log 2>~/data/Kmer41/ass.err
-./SOAPdenovo-127mer all -s ../soap.contig.txt -p 48 -K 41 -R -M 2 -o M2/graph_41 1>ass.log 2>ass.err
-./SOAPdenovo-127mer all -s ../soap.contig.txt -p 48 -K 41 -R -M 3 -o M3/graph_41 1>ass.log 2>ass.err
-./SOAPdenovo-127mer all -s ../soap.contig.txt -p 48 -K 43 -R -o graph_43 1>ass.log 2>ass.err
-./SOAPdenovo-127mer all -s ../soap.contig.txt -p 48 -K 43 -R -M 2 -o M2/graph_43 1>ass.log 2>ass.err
-./SOAPdenovo-127mer all -s ../soap.contig.txt -p 48 -K 43 -R -M 3 -o M3/graph_43 1>ass.log 2>ass.err
-./SOAPdenovo-127mer all -s ../soap.contig.txt -p 48 -K 47 -R -o graph_47 1>ass.log 2>ass.err
-./SOAPdenovo-127mer all -s ../soap.contig.txt -p 48 -K 53 -R -o graph_53 1>ass.log 2>ass.err
-./SOAPdenovo-127mer all -s ../soap.contig.txt -p 48 -K 63 -R -o graph_63 1>ass.log 2>ass.err
-./SOAPdenovo-127mer all -s ../soap.contig.txt -p 48 -K 73 -R -o graph_73 1>ass.log 2>ass.err
-./SOAPdenovo-127mer all -s ../soap.contig.txt -p 48 -K 83 -R -o graph_83 1>ass.log 2>ass.err
-./SOAPdenovo-127mer all -s ../soap.contig.txt -p 48 -K 97 -R -o graph_97 1>ass.log 2>ass.err
-./SOAPdenovo-127mer all -s ../soap.contig.txt -p 48 -K 107 -R -o graph_107 1>ass.log 2>ass.err
-./SOAPdenovo-127mer all -s ../soap.contig.txt -p 48 -K 117 -R -o graph_117 1>ass.log 2>ass.err
+./SOAPdenovo-127mer all -s ../soap.contig.txt -p 48 -K 41 -R -o ~/data/Kmer41/M1/graph_41 1>~/data/Kmer41/M1/ass.log 2>~/data/Kmer41/M1/ass.err
+./SOAPdenovo-127mer all -s ../soap.contig.txt -p 48 -K 41 -R -M 2 -o ~/data/Kmer41/M2/graph_41 1>~/data/Kmer41/M2/ass.log 2>~/data/Kmer41/M2/ass.err
+./SOAPdenovo-127mer all -s ../soap.contig.txt -p 48 -K 41 -R -M 3 -o ~/data/Kmer41/M3/graph_41 1>~/data/Kmer41/M3/ass.log 2>~/data/Kmer41/M3/ass.err
+./SOAPdenovo-127mer all -s ../soap.contig.txt -p 48 -K 43 -R -o ~/data/Kmer43/M1/graph_43 1>~/data/Kmer43/M1/ass.log 2>~/data/Kmer43/M1/ass.err
+./SOAPdenovo-127mer all -s ../soap.contig.txt -p 48 -K 43 -R -M 2 -o ~/data/Kmer43/M2/graph_43 1>~/data/Kmer43/M2/ass.log 2>~/data/Kmer43/M2/ass.err
+./SOAPdenovo-127mer all -s ../soap.contig.txt -p 48 -K 43 -R -M 3 -o ~/data/Kmer43/M3/graph_43 1>~/data/Kmer43/M3/ass.log 2>~/data/Kmer43/M3/ass.err
+./SOAPdenovo-127mer all -s ../soap.contig.txt -p 48 -K 47 -R -o ~/data/Kmer47/graph_47 1>~/data/Kmer47/ass.log 2>~/data/Kmer47/ass.err
+./SOAPdenovo-127mer all -s ../soap.contig.txt -p 48 -K 53 -R -o ~/data/Kmer53/graph_53 1>~/data/Kmer53/ass.log 2>~/data/Kmer53/ass.err
+./SOAPdenovo-127mer all -s ../soap.contig.txt -p 48 -K 63 -R -o ~/data/Kmer63/graph_63 1>~/data/Kmer63/ass.log 2>~/data/Kmer63/ass.err
+./SOAPdenovo-127mer all -s ../soap.contig.txt -p 48 -K 73 -R -o ~/data/Kmer73/graph_73 1>~/data/Kmer73/ass.log 2>~/data/Kmer73/ass.err
+./SOAPdenovo-127mer all -s ../soap.contig.txt -p 48 -K 83 -R -o ~/data/Kmer83/graph_83 1>~/data/Kmer83/ass.log 2>~/data/Kmer83/ass.err
+./SOAPdenovo-127mer all -s ../soap.contig.txt -p 48 -K 97 -R -o ~/data/Kmer97/graph_97 1>~/data/Kmer97/ass.log 2>~/data/Kmer97/ass.err
+./SOAPdenovo-127mer all -s ../soap.contig.txt -p 48 -K 107 -R -o ~/data/Kmer107/graph_107 1>~/data/Kmer107/ass.log 2>~/data/Kmer107/ass.err
+./SOAPdenovo-127mer all -s ../soap.contig.txt -p 48 -K 117 -R -o ~/data/Kmer117/graph_117 1>~/data/Kmer117/ass.log 2>~/data/Kmer117/ass.err
