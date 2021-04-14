bf2c7b2c20c2f754ba71e9cdc712943bb2e0db97
Rong Guo <33555113+rguo1990@users.noreply.github.com>
2021-04-14 16:52:07 -0400

    Update macrosynteny_Bmori.sh

--- macrosynteny_Bmori.sh
+++ macrosynteny_Bmori.sh
@@ -1,5 +1,8 @@
 ## the script was used to make macrosynteny analysis for all super-scaffolded assemblies with B. mori
 
 ~/Rong/MUMmer/MUMmer3.23/promer --mum ~/Rong/Ragout/output_Hsub_unsolid_2/Hsub_ragout_unsolid.fasta ~/Rong/H_vir/Bmori/chromosome.fa
-~/Rong/MUMmer/MUMmer3.23/mummerplot -t png --layout (--fat) -R reference.fasta -Q query.fasta out.delta --large
+~/Rong/MUMmer/MUMmer3.23/mummerplot -t png --fat out.delta --large
+
+~/Rong/MUMmer/MUMmer3.23/promer --mum ~/Rong/Ragout/output_Hsub_solid_2/Hsub_ragout_solid.fasta ~/Rong/H_vir/Bmori/chromosome.fa
+~/Rong/MUMmer/MUMmer3.23/mummerplot -t png --fat out.delta --large
 
