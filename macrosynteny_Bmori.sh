ffc58fe8e49fbe8ac36521c6ffdc041b88f074b3
Rong Guo <33555113+rguo1990@users.noreply.github.com>
2021-04-13 21:58:40 -0400

    Create macrosynteny_Bmori.sh

---
+++ macrosynteny_Bmori.sh
@@ -0,0 +1,3 @@
+## the script was used to make macrosynteny analysis for all super-scaffolded assemblies with B. mori
+
+~/Rong/MUMmer/MUMmer3.23/promer --mum ~/Rong/Ragout/output_Hsub_unsolid_2/Hsub_ragout_unsolid.fasta ~/Rong/H_vir/Bmori/chromosome.fa
