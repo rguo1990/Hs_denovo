f041502ee1d2d78ef42cf4ac9314aaa6c67512fd
Rong Guo <33555113+rguo1990@users.noreply.github.com>
2021-04-17 01:29:13 -0400

    Update macrosynteny_Bmori.sh

--- macrosynteny_Bmori.sh
+++ macrosynteny_Bmori.sh
@@ -5,7 +5,7 @@ cd /media/megan/easystore/Rong/Hsub_genome_assembly/macrosynteny_Bmori
 ~/Rong/MUMmer/mummer-4.0.0beta5/promer_test --mum --mincluster=200 ~/Rong/Ragout/output_Hsub_unsolid_2/Hsub_ragout_unsolid.fasta ~/Rong/H_vir/Bmori/chromosome.fa
 ~/Rong/MUMmer/mummer-4.0.0beta5/mummerplot -t png --fat out.delta --large
 
-~/Rong/MUMmer/mummer-4.0.0beta5/promer_test --mum --mincluster=200 ~/Rong/Ragout/output_Hsub_solid_2/Hsub_ragout_solid.fasta ~/Rong/H_vir/Bmori/chromosome.fa
+~/Rong/MUMmer/mummer-4.0.0beta5/promer_test --mum --mincluster=200 ~/Rong/Ragout/output_Hsub_solid/Hsub_ragout_solid.fasta ~/Rong/H_vir/Bmori/chromosome.fa
 ~/Rong/MUMmer/mummer-4.0.0beta5/mummerplot -t png --fat out.delta --large
 
 ~/Rong/MUMmer/mummer-4.0.0beta5/promer_test --mum --mincluster=200 ~/Rong/Ragout/output_Hzea_unsolid/Hzea_ragout_unsolid.fasta ~/Rong/H_vir/Bmori/chromosome.fa
