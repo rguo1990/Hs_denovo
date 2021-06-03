59063e096a778fbe8b4efa6a33f176bb12976d98
Rong Guo <33555113+rguo1990@users.noreply.github.com>
2021-06-03 11:44:50 -0400

    Update run_Ragout2_Hsub.sh

--- run_Ragout2_Hsub.sh
+++ run_Ragout2_Hsub.sh
@@ -15,6 +15,7 @@ awk '/^>/ {print ">Hvir.scaffold." ++i; next} {print}' < Hvir_Dovetail_softmask.
 
 cd ~/Rong/SibeliaZ/SibeliaZ/SibeliaZ-LCB/bin
 ./sibeliaz -f 30 ~/Rong/Ragout/Hsub_relabel_geno.chr.fasta ~/Rong/Ragout/Hvir_Dovetail_relabel_geno.chr.fasta ~/Rong/Ragout/Hzea_relabel_geno.chr.fasta ~/Rong/Ragout/Harm_relabel_geno.chr.fasta
+## results are saved in ~/Rong/SibeliaZ/SibeliaZ/SibeliaZ-LCB/bin/sibeliaz_out_Hsub
 
 ## to use Ragout2, the sequence header of input fasta files should be in a simple format: >chromosome.#
 awk '/^>/ {print ">scaffold." ++i; next} {print}' < Hvir_Dovetail_softmask.fasta > Hvir_Dovetail_relabel.fasta
