513dc5ae4902262709e1d7a9219569181dfd81c2
Rong Guo <33555113+rguo1990@users.noreply.github.com>
2021-06-19 23:29:16 -0400

    Update macrosynteny_Bmori.sh

--- macrosynteny_Bmori.sh
+++ macrosynteny_Bmori.sh
@@ -2,22 +2,22 @@
 
 cd /media/megan/easystore/Rong/Hsub_genome_assembly/macrosynteny_Bmori
 
-~/Rong/MUMmer/mummer-4.0.0beta5/promer_test --mum --mincluster=200 ~/Rong/Ragout/output_Hsub_unsolid_2/Hsub_ragout_unsolid.fasta ~/Rong/H_vir/Bmori/chromosome.fa
-~/Rong/MUMmer/mummer-4.0.0beta5/mummerplot -t png --fat out.delta --large
+~/Rong/MUMmer/mummer-4.0.0beta5/promer_test --mum --mincluster=200 ~/Rong/Ragout/output_Hsub_unsolid_2/Hsub_ragout_unsolid.fasta ~/Rong/H_vir/Bmori/chromosome.fa -p Hsub_unsolid_Bmori
+~/Rong/MUMmer/mummer-4.0.0beta5/mummerplot -t png --fat Hsub_unsolid_Bmori.delta -p Hsub_unsolid_Bmori --large
 
-~/Rong/MUMmer/mummer-4.0.0beta5/promer_test --mum --mincluster=200 ~/Rong/Ragout/output_Hsub_solid/Hsub_ragout_solid.fasta ~/Rong/H_vir/Bmori/chromosome.fa
-~/Rong/MUMmer/mummer-4.0.0beta5/mummerplot -t png --fat out.delta --large
+~/Rong/MUMmer/mummer-4.0.0beta5/promer_test --mum --mincluster=200 ~/Rong/Ragout/output_Hsub_solid/Hsub_ragout_solid.fasta ~/Rong/H_vir/Bmori/chromosome.fa -p Hsub_solid_Bmori
+~/Rong/MUMmer/mummer-4.0.0beta5/mummerplot -t png --fat Hsub_solid_Bmori.delta -p Hsub_solid_Bmori --large
 
-~/Rong/MUMmer/mummer-4.0.0beta5/promer_test --mum --mincluster=200 ~/Rong/Ragout/output_Hzea_unsolid/Hzea_ragout_unsolid.fasta ~/Rong/H_vir/Bmori/chromosome.fa
-~/Rong/MUMmer/mummer-4.0.0beta5/mummerplot -t png --fat out.delta --large
+~/Rong/MUMmer/mummer-4.0.0beta5/promer_test --mum --mincluster=200 ~/Rong/Ragout/output_Hzea_unsolid/Hzea_ragout_unsolid.fasta ~/Rong/H_vir/Bmori/chromosome.fa -p Hzea_unsolid_Bmori
+~/Rong/MUMmer/mummer-4.0.0beta5/mummerplot -t png --fat Hzea_unsolid_Bmori.delta -p Hzea_unsolid_Bmori --large
 
-~/Rong/MUMmer/mummer-4.0.0beta5/promer_test --mum --mincluster=200 ~/Rong/Ragout/output_Hzea_solid/Hzea_ragout_solid.fasta ~/Rong/H_vir/Bmori/chromosome.fa
-~/Rong/MUMmer/mummer-4.0.0beta5/mummerplot -t png --fat out.delta --large
+~/Rong/MUMmer/mummer-4.0.0beta5/promer_test --mum --mincluster=200 ~/Rong/Ragout/output_Hzea_solid/Hzea_ragout_solid.fasta ~/Rong/H_vir/Bmori/chromosome.fa -p Hzea_solid_Bmori
+~/Rong/MUMmer/mummer-4.0.0beta5/mummerplot -t png --fat Hzea_solid_Bmori.delta -p Hzea_solid_Bmori --large
 
-~/Rong/MUMmer/mummer-4.0.0beta5/promer_test --mum --mincluster=200 ~/Rong/Ragout/output_Harm_unsolid/Harm_ragout_unsolid.fasta ~/Rong/H_vir/Bmori/chromosome.fa
-~/Rong/MUMmer/mummer-4.0.0beta5/mummerplot -t png --fat out.delta --large
+~/Rong/MUMmer/mummer-4.0.0beta5/promer_test --mum --mincluster=200 ~/Rong/Ragout/output_Harm_unsolid/Harm_ragout_unsolid.fasta ~/Rong/H_vir/Bmori/chromosome.fa -p Harm_unsolid_Bmori
+~/Rong/MUMmer/mummer-4.0.0beta5/mummerplot -t png --fat Harm_unsolid_Bmori.delta -p Harm_unsolid_Bmori --large
 
-~/Rong/MUMmer/mummer-4.0.0beta5/promer_test --mum --mincluster=200 ~/Rong/Ragout/output_Harm_solid/Harm_ragout_solid.fasta ~/Rong/H_vir/Bmori/chromosome.fa
-~/Rong/MUMmer/mummer-4.0.0beta5/mummerplot -t png --fat out.delta --large
+~/Rong/MUMmer/mummer-4.0.0beta5/promer_test --mum --mincluster=200 ~/Rong/Ragout/output_Harm_solid/Harm_ragout_solid.fasta ~/Rong/H_vir/Bmori/chromosome.fa -p Harm_solid_Bmori
+~/Rong/MUMmer/mummer-4.0.0beta5/mummerplot -t png --fat Harm_solid_Bmori.delta -p Harm_solid_Bmori --large
 
 
