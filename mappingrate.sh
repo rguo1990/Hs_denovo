7e4dd067120fcb801836267dc8c4eb2dd8904d65
Rong Guo <33555113+rguo1990@users.noreply.github.com>
2022-03-15 11:48:25 -0400

    Add files via upload

---
+++ mappingrate.sh
@@ -0,0 +1,33 @@
+## the script was used to align the paired end library SRR5463746 back to the H.virescens assemblies, and calculate mapping back rate
+
+## randomly subset the fastq files into 5 datasets, and each has 10% of the original data
+python randomReadSubSample.py -f1 SRR5463746_1.fastq -f2 SRR5463746_2.fastq -n 5 -r 0 -s 0.1 -o SRR5463746_mappingback0.1
+
+## mapping the reads back to the assemblies
+## to the published H.virescens assembly
+bowtie2 -x ~/Rong/H_vir/H_vir -1 SRR5463746_mappingback0.1_0.1.fq -2 SRR5463746_mappingback0.1_0.2.fq -p 15 1> mappingback0.1_0.sam 2> mappingback0.1_0.report
+bowtie2 -x ~/Rong/H_vir/H_vir -1 SRR5463746_mappingback0.1_1.1.fq -2 SRR5463746_mappingback0.1_1.2.fq -p 15 1> mappingback0.1_1.sam 2> mappingback0.1_1.report
+bowtie2 -x ~/Rong/H_vir/H_vir -1 SRR5463746_mappingback0.1_2.1.fq -2 SRR5463746_mappingback0.1_2.2.fq -p 15 1> mappingback0.1_2.sam 2> mappingback0.1_2.report
+bowtie2 -x ~/Rong/H_vir/H_vir -1 SRR5463746_mappingback0.1_3.1.fq -2 SRR5463746_mappingback0.1_3.2.fq -p 15 1> mappingback0.1_3.sam 2> mappingback0.1_3.report
+bowtie2 -x ~/Rong/H_vir/H_vir -1 SRR5463746_mappingback0.1_4.1.fq -2 SRR5463746_mappingback0.1_4.2.fq -p 15 1> mappingback0.1_4.sam 2> mappingback0.1_4.report
+
+## to the Dovetail assembly
+bowtie2 -x ~/Rong/H_vir/Dovetail -1 SRR5463746_mappingback0.1_0.1.fq -2 SRR5463746_mappingback0.1_0.2.fq -p 15 1> mappingback0.1_Dovetail.0.sam 2> mappingback0.1_Dovetail.0.report
+bowtie2 -x ~/Rong/H_vir/Dovetail -1 SRR5463746_mappingback0.1_1.1.fq -2 SRR5463746_mappingback0.1_1.2.fq -p 15 1> mappingback0.1_Dovetail.1.sam 2> mappingback0.1_Dovetail.1.report
+bowtie2 -x ~/Rong/H_vir/Dovetail -1 SRR5463746_mappingback0.1_2.1.fq -2 SRR5463746_mappingback0.1_2.2.fq -p 15 1> mappingback0.1_Dovetail.2.sam 2> mappingback0.1_Dovetail.2.report
+bowtie2 -x ~/Rong/H_vir/Dovetail -1 SRR5463746_mappingback0.1_3.1.fq -2 SRR5463746_mappingback0.1_3.2.fq -p 15 1> mappingback0.1_Dovetail.3.sam 2> mappingback0.1_Dovetail.3.report
+bowtie2 -x ~/Rong/H_vir/Dovetail -1 SRR5463746_mappingback0.1_4.1.fq -2 SRR5463746_mappingback0.1_4.2.fq -p 15 1> mappingback0.1_Dovetail.4.sam 2> mappingback0.1_Dovetail.4.report
+
+## to the H.virescens solid assembly
+bowtie2 -x ~/Rong/H_vir/ragout_solid -1 SRR5463746_mappingback0.1_0.1.fq -2 SRR5463746_mappingback0.1_0.2.fq -p 15 1> mappingback0.1_ragout_solid.0.sam 2> mappingback0.1_ragout_solid.0.report
+bowtie2 -x ~/Rong/H_vir/ragout_solid -1 SRR5463746_mappingback0.1_1.1.fq -2 SRR5463746_mappingback0.1_1.2.fq -p 15 1> mappingback0.1_ragout_solid.1.sam 2> mappingback0.1_ragout_solid.1.report
+bowtie2 -x ~/Rong/H_vir/ragout_solid -1 SRR5463746_mappingback0.1_2.1.fq -2 SRR5463746_mappingback0.1_2.2.fq -p 15 1> mappingback0.1_ragout_solid.2.sam 2> mappingback0.1_ragout_solid.2.report
+bowtie2 -x ~/Rong/H_vir/ragout_solid -1 SRR5463746_mappingback0.1_3.1.fq -2 SRR5463746_mappingback0.1_3.2.fq -p 15 1> mappingback0.1_ragout_solid.3.sam 2> mappingback0.1_ragout_solid.3.report
+bowtie2 -x ~/Rong/H_vir/ragout_solid -1 SRR5463746_mappingback0.1_4.1.fq -2 SRR5463746_mappingback0.1_4.2.fq -p 15 1> mappingback0.1_ragout_solid.4.sam 2> mappingback0.1_ragout_solid.4.report
+
+## to the H.virescens unsolid assembly
+bowtie2 -x ~/Rong/H_vir/ragout_unsolid -1 SRR5463746_mappingback0.1_0.1.fq -2 SRR5463746_mappingback0.1_0.2.fq -p 15 1> mappingback0.1_ragout_unsolid.0.sam 2> mappingback0.1_ragout_unsolid.0.report
+bowtie2 -x ~/Rong/H_vir/ragout_unsolid -1 SRR5463746_mappingback0.1_1.1.fq -2 SRR5463746_mappingback0.1_1.2.fq -p 15 1> mappingback0.1_ragout_unsolid.1.sam 2> mappingback0.1_ragout_unsolid.1.report
+bowtie2 -x ~/Rong/H_vir/ragout_unsolid -1 SRR5463746_mappingback0.1_2.1.fq -2 SRR5463746_mappingback0.1_2.2.fq -p 15 1> mappingback0.1_ragout_unsolid.2.sam 2> mappingback0.1_ragout_unsolid.2.report
+bowtie2 -x ~/Rong/H_vir/ragout_unsolid -1 SRR5463746_mappingback0.1_3.1.fq -2 SRR5463746_mappingback0.1_3.2.fq -p 15 1> mappingback0.1_ragout_unsolid.3.sam 2> mappingback0.1_ragout_unsolid.3.report
+bowtie2 -x ~/Rong/H_vir/ragout_unsolid -1 SRR5463746_mappingback0.1_4.1.fq -2 SRR5463746_mappingback0.1_4.2.fq -p 15 1> mappingback0.1_ragout_unsolid.4.sam 2> mappingback0.1_ragout_unsolid.4.report
