c59256afa111d68922b0af06d03fe9deed413023
Rong Guo <33555113+rguo1990@users.noreply.github.com>
2022-03-21 14:35:11 -0400

    Update Figure 4.r

--- Figure 4.r
+++ Figure 4.r
@@ -5,6 +5,8 @@ N50 <- c(201477,6100690,8865798,1000414,9477009,9240022)
 completeness <- c(0.964,0.966,0.963,0.974,0.974,0.971)
 data <- rbind(N_50,completeness)
 colnames(data) <- names
+png("Figure 4.png",units="in", width=9, height=5,res=300)
+par(mar=c(5, 4, 4, 8), xpd=TRUE)
 plot(x = 1:6, y = N50,col = "red",pch = 19,xaxt = "n",type = "p",xlab = "Assemblies",main="Improved N50 Values and Completeness")
 text(x=2:6,y=N50[2:6],labels = N50[2:6],cex=0.5,pos=1,col="red")
 text(x=1,y=N50[1],labels=N50[1],cex=0.5,pos=4,col="red")
@@ -14,5 +16,6 @@ text(x=1:6,y=completeness,labels = completeness,cex=0.5,pos=3,col="blue")
 axis(side = 1,at=c(1,2,3,4,5,6),labels = names)
 axis(side = 4,at=c(0.9,0.92,0.94,0.96,0.98,1))
 mtext("Completeness", side = 4,line = -1)
-legend("bottomright", c("N50", "Completeness"), cex =0.8,
+legend("bottomright", inset = c(-0.24, 0),xpd = TRUE, c("N50", "Completeness"), cex =0.8,
        col = c("red", "blue"), pch = c(19, 17))
+dev.off()
