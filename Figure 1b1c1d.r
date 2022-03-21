4f25f7a47988cde7c1a7253a2ae44dbcf7e480c8
Rong Guo <33555113+rguo1990@users.noreply.github.com>
2022-03-21 14:26:33 -0400

    Update Figure 1b1c1d.r

--- Figure 1b1c1d.r
+++ Figure 1b1c1d.r
@@ -14,6 +14,7 @@ ratio <- lepidoptera$`Total Genome Size`/lepidoptera$`Scaffold N50`
 lepidoptera_log10 <- log10(lepidoptera$`Scaffold N50`)
 
 ## plot figure 1b -- contiguities of Lepidopteran assembled genomes
+png("Figure 1b.png",units="in", width=11, height=5,res=300)
 ggplot(data=lepidoptera,aes(x=`Family`,y=lepidoptera_log10,fill=`Family`)) + geom_boxplot(varwidth = TRUE) + geom_point(col = "black") +
   labs(x="Lepidoptera families",y="log10(N50)",title="Contiguities of Lepidopteran Assembled Genomes") + theme_classic() +
   theme(plot.title = element_text(hjust = 0.5)) +
@@ -21,18 +22,23 @@ ggplot(data=lepidoptera,aes(x=`Family`,y=lepidoptera_log10,fill=`Family`)) + geo
   theme(axis.title.x = element_text(size = 15, face = "bold")) +
   theme(axis.title.y = element_text(size = 15, face = "bold")) +
   theme(title = element_text(size = 18, face = "bold")) 
+dev.off()
   
 ## plot figure 1c -- N50 vs. Total Genome Size
 sh <- c(1,2,2,2,3,4,4,4,5,6,7,8,9,9,9,10,11,12,rep(13,11),rep(14,32),rep(15,8),rep(16,6),17,18,19,19,19,20,20,21,22,22,23,23)
 d <- cbind(lepidoptera,sh)
 family <- lepidoptera$Family
+png("Figure 1c.png",units="in", width=8, height=5,res=300)
+par(mar=c(5, 4, 4, 8), xpd=TRUE)
 plot(x=d$`Total Genome Size`,y=lepidoptera_log10,col = c("sienna","bisque2","blue","brown","coral","orange","darkblue",
                                                          "gold","gray","cyan","pink","yellow","maroon","navy","green","purple",
                                                          "darkred","seashell","tan","violet","yellowgreen","hotpink","black")[d$sh],pch=19,
      xlab = "Total Genome Size",ylab="log10(N50)",main="N50 vs. Total Genome Size")
-legend("bottomright",legend = unique(family),col = c("sienna","bisque2","blue","brown","coral","orange","darkblue",
+legend("bottomright",inset = c(-0.2, 0),xpd = TRUE,bty="n",legend = unique(family),col = c("sienna","bisque2","blue","brown","coral","orange","darkblue",
                                                      "gold","gray","cyan","pink","yellow","maroon","navy","green","purple",
-                                                     "darkred","seashell","tan","violet","yellowgreen","hotpink","black"),pch = 19,cex=0.4,ncol = 2,text.font = 2)
+                                                     "darkred","seashell","tan","violet","yellowgreen","hotpink","black"),pch = 19,cex=0.66,ncol = 1,text.font = 3)
+
+dev.off()
 
 ## the range of completeness
 min(lepidoptera$`C (%)`)
@@ -41,6 +47,7 @@ mean(lepidoptera$`C (%)`)
 median(lepidoptera$`C (%)`)
 
 ## plot figure 1d -- completeness of Lepidopteran assembled genomes
+png("Figure 1d.png",units="in", width=11, height=5,res=300)
 ggplot(data=lepidoptera,aes(x=Family,y=`C (%)`,fill=`Family`)) + geom_boxplot(varwidth = TRUE) + geom_point(col="black") +
   labs(x="Lepidoptera families",y="Completeness (%)",title="Completeness of Lepidopteran Assembled Genomes") + theme_classic() +
   theme(plot.title = element_text(hjust = 0.5)) +
@@ -48,3 +55,4 @@ ggplot(data=lepidoptera,aes(x=Family,y=`C (%)`,fill=`Family`)) + geom_boxplot(va
   theme(axis.title.x = element_text(size = 15, face = "bold")) +
   theme(axis.title.y = element_text(size = 15, face = "bold")) +
   theme(title = element_text(size = 18, face = "bold")) 
+dev.off()
